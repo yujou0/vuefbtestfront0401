5430605ff40988df029d8368fd782157fe5a504d
yujou0 <63997329+yujou0@users.noreply.github.com>
2021-04-01 14:31:00 +0800

    Update vue.config.js

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-    devServer: {
-        open: process.platform === 'darwin',
-        host: '0.0.0.0',
-        port: 8085, // CHANGE YOUR PORT HERE!
-        https: true,
-        hotOnly: false,
-    },
-}
+    publicPath: './'
+  }
